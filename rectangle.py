66be31e8926a089424296ad6ea02dd6d29872203
ErotyScrtipt <misha585779@gmail.com>
2024-10-03 00:59:37 +0300

    Добавил комментарии для функций прямоугольника

--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,24 @@
 def area(a, b): 
+    '''
+        Считает площадь прямоугольника по заданным сторонам
+        :param a: длина прямоугольника
+        :param b: ширина прямоугольника
+        :type a: float
+        :type b: float
+        :return: площадь прямоугольника
+        :rtype: float
+    '''
     return a * b 
 
 def perimeter(a, b): 
+    '''
+        Считает периметр прямоугольника по заданным сторонам
+        :param a: длина прямоугольника
+        :param b: ширина прямоугольника 
+        :type a: float
+        :type b: float 
+        :return: периметр прямоугольника
+        :rtype: float
+    '''
     return (a + b) * 2
 
