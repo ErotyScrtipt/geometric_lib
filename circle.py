b4c02f3fa4e5fc9ffcc41b8e3144873fded9c1f4
ErotyScrtipt <misha585779@gmail.com>
2024-10-03 00:58:00 +0300

    Добавил комментарии для функций круга

--- circle.py
+++ circle.py
@@ -2,9 +2,23 @@ import math
 
 
 def area(r):
+    '''
+        Считает площадь круга по заданному радиусу
+        :param r: радиус круга
+        :type r: float
+        :return: площадь круга
+        :rtype: float
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+        Считает периметр круга по заданному радиусу
+        :param r: радиус круга
+        :type r: float
+        :return: периметр круга
+        :rtype: float 
+    '''
     return 2 * math.pi * r
 
