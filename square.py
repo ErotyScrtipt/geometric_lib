1606fd5451d096bf18051e1f68d486f178d36c47
ErotyScrtipt <misha585779@gmail.com>
2024-10-03 01:02:04 +0300

    Добавил комментарии для функций квадрата и треугольника

--- square.py
+++ square.py
@@ -1,7 +1,21 @@
 
 def area(a):
+    '''
+        Считает площадь квадрата
+        :param a: длина стороны квадрата
+        :type a: float
+        :return: площадь квадрата
+        :rtype: float
+    '''
     return a * a
 
 
 def perimeter(a):
+    '''
+        Считает периметр квадрата
+        :param a: длина стороны квадрата
+        :type a: float
+        :return: периметр квадрата
+        :rtype: float
+    '''
     return 4 * a
--- triangle.py
+++ triangle.py
@@ -1,6 +1,26 @@
 def area(a, h): 
+    '''
+        Считает площадь треугольника по заданным сторонам
+        :param a: длина стороны треугольника
+        :param h: высота треугольника
+        :type a: float
+        :type h: float
+        :return: площадь треугольника
+        :rtype: float
+    '''
     return a * h / 2 
 
 def perimeter(a, b, c): 
+    '''
+        Считает периметр треугольника по заданным сторонам
+        :param a: длина стороны треугольника
+        :param b: длина стороны треугольника
+        :param c: длина стороны треугольника
+        :type a: float
+        :type b: float
+        :type c: float
+        :return: периметр треугольника
+        :rtype: float
+    '''
     return a + b + c 
 
